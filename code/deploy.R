45e89d2bb01036d98fe3f29a16468dea904ca2de
adhampton110 <anthony.hampton1235@gmail.com>
2024-09-29 16:32:07 -0500

    adding dplyr to deploy.R

--- code/deploy.R
+++ code/deploy.R
@@ -1,6 +1,7 @@
 # deploy.R handles automated deployment for MeltShiny to shinyapps (hosting).
 
 library(rsconnect)
+library(dplyr)
 
 # A function to stop the script when one of the variables cannot be found and to strip quotation marks from the secrets when you supplied them
 error_on_missing_name <- function(name) {
